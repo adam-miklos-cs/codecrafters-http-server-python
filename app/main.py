6cdd065f60ccf2c76b1246333c3d9ae45519ca45
Ádám Miklós <adam.miklos.cs@gmail.com>
2026-07-17 14:11:06 +0200

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -4,14 +4,60 @@ import argparse
 import os
 
 
+def handle_get(target, headers, args):
+    if target == "/":
+        return b"HTTP/1.1 200 OK\r\n\r\n"
+
+    if target.startswith("/echo/"):
+        echo_str = target.split("/echo/", 1)[1]
+        response_text = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo_str)}\r\n\r\n{echo_str}"
+        return response_text.encode("utf-8")
+
+    if target == "/user-agent":
+        for header in headers:
+            if header.lower().startswith(b"user-agent:"):
+                user_agent = header.decode("utf-8").split(":", 1)[1].strip()
+                response_text = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
+                return response_text.encode("utf-8")
+
+        return b"HTTP/1.1 400 Bad Request\r\n\r\n"
+
+    if target.startswith("/files/"):
+        file_name = target.split("/files/", 1)[1]
+        full_path = os.path.join(args.directory, file_name)
+
+        if os.path.isfile(full_path):
+            with open(full_path, "rb") as file:
+                file_content = file.read()
+                response_text = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(file_content)}\r\n\r\n"
+                return response_text.encode("utf-8") + file_content
+
+    return b"HTTP/1.1 404 Not Found\r\n\r\n"
+
+
+def handle_post(target, headers, body, args):
+    if target.startswith("/files/"):
+        file_name = target.split("/files/", 1)[1]
+
+        full_path = os.path.join(args.directory, file_name)
+
+        with open(full_path, "wb") as file:
+            file.write(body)
+
+        return b"HTTP/1.1 201 Created\r\n\r\n"
+
+    return b"HTTP/1.1 404 Not Found\r\n\r\n"
+
+
 def handle_client(client_socket, args):
     try:
-        # Receive data from the client socket
         DATA_LENGTH = 1024
         raw_request_bytes = client_socket.recv(DATA_LENGTH)
 
-        [head, body] = raw_request_bytes.split(b"\r\n\r\n", 1)
+        if not raw_request_bytes:
+            return
 
+        [head, body] = raw_request_bytes.split(b"\r\n\r\n", 1)
         head_lines = head.split(b"\r\n")
 
         request_line = head_lines[0]
@@ -19,36 +65,18 @@ def handle_client(client_socket, args):
 
         [method, target, version] = request_line.decode("utf-8").split(" ")
 
-        if method == "GET":
-            response = b"HTTP/1.1 404 Not Found\r\n\r\n"
+        routes = {"GET": handle_get, "POST": handle_post}
 
-            if target == "/":
-                response = b"HTTP/1.1 200 OK\r\n\r\n"
+        if method in routes:
+            if method == "POST":
+                response = routes[method](target, headers, body, args)
+            else:
+                response = routes[method](target, headers, args)
+        else:
+            response = b"HTTP/1.1 405 Method Not Allowed\r\n\r\n"
 
-            elif target.startswith("/echo/"):
-                echo_str = target.split("/echo/", 1)[1]
-                response_text = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(echo_str)}\r\n\r\n{echo_str}"
-                response = response_text.encode("utf-8")
+        client_socket.sendall(response)
 
-            elif target.startswith("/user-agent"):
-                for header in headers:
-                    print(header.lower().decode("utf-8"))
-                    if header.lower().startswith(b"user-agent:"):
-                        user_agent = header.decode("utf-8").split(":", 1)[1].strip()
-                        response_text = f"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {len(user_agent)}\r\n\r\n{user_agent}"
-                        response = response_text.encode("utf-8")
-                        break
-
-            elif target.startswith("/files/"):
-                file_name = target.split("/files/", 1)[1]
-                full_path = os.path.join(args.directory, file_name)
-                if os.path.isfile(full_path):
-                    with open(full_path, "rb") as file:
-                        file_content = file.read()
-                        response_text = f"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: {len(file_content)}\r\n\r\n"
-                        response = response_text.encode("utf-8") + file_content
-
-            client_socket.sendall(response)
     finally:
         client_socket.close()
 
@@ -67,7 +95,7 @@ def main():
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
 
     while True:
-        client_socket, client_address = server_socket.accept()  # wait for client
+        client_socket, client_address = server_socket.accept()
         client_thread = threading.Thread(
             target=handle_client, args=(client_socket, args)
         )
